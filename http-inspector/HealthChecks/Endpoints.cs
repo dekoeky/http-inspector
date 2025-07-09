a17b00ce0a0852a724853d0a22a8ca802f69a865
Wesley Van Koeckhoven <wvk@outlook.be>
2025-07-10 00:46:04 +0200

    fix endpoint separator

--- http-inspector/HealthChecks/Endpoints.cs
+++ http-inspector/HealthChecks/Endpoints.cs
@@ -2,7 +2,7 @@ namespace HttpInspector.HealthChecks;
 
 internal static class Endpoints
 {
-    public const string Health = "health";
+    public const string Health = "/health";
     public const string HealthExplain = $"{Health}/explain";
     public const string HealthLive = $"{Health}/live";
     public const string HealthReady = $"{Health}/ready";
